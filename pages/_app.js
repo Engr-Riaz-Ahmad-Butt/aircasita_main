6eed1c993dbef13f4058efea97d9753d39678fd2
osama-shakil <72553062+osama-shakil@users.noreply.github.com>
2024-01-09 08:54:23 +0500

    stepuup with tailwind done

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 import "../assets/scss/index.scss";
-
 import Default from "../layout/Default";
 import Providers from "../store/Providers";
+
 
 // export default function App({ Component, pageProps }) {
 //   return <Component {...pageProps} />
 // }
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {``
   // Use the layout defined at the page level, if available
   const Layout = Component.Layout || Default;
-  // useEffect(() => {
-  //   require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  // }, []);
+  useEffect(() => {
+    require("../assets/scss/index.scss");
+  }, []);
 
   return (
     <Providers>
