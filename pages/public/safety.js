196ba20a9856d10be4aa73095f01a44cfe5ba804
Riaz <riazahmadbutt01@gmail.com>
2024-01-09 18:02:02 +0500

    created page for the safety of guests, created a page for house rules also make responsive

---
+++ pages/public/safety.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const safety = () => {
+  return (
+    <div class="md:container md:mx-auto">
+      <h1 className="text-3xl font-semibold p-3">
+        airCASITA - Safety for Guests:
+      </h1>
+      <p className="pt-3 sm:p-3">
+        Hosts are reminded to survey their property frequently to identify and
+        mitigate risk. Spotting dangers and removing them or putting up warning
+        signs, these preventative actions by the host can avoid a guest fall in
+        the bath or shower, a stumble on the stairs, a head-bumping at a low
+        ceiling, a toxic exposure, or other common injury. Landlords are advised
+        to take such reasonable pre-emptive action.
+        <br></br> <br></br>A tenant who suffers an injury on the premises of a
+        rental property may argue that they were injured because the landlord
+        was careless (or “negligent,” in legal terms). This argument will have
+        less weight if the landlord acts reasonably to warn about and mitigate
+        risks beforehand. Guests can be asked to sign a disclosure which admits
+        that the guest was told of certain potentially risky circumstances at
+        the rental property and had agreed to cooperate to avoid injury to the
+        degree possible.<br></br> <br></br>
+        The reasonable landlord will understand that the cost to mitigate a risk
+        is typically much less than a legal battle to explain or deflect the
+        liability of not doing so when a tenant is injured. Tenants who are hurt
+        in accidents on the landlord’s property usually have the option to bring
+        a personal injury claim against the landlord for compensation. Landlords
+        and tenants can often resolve these matters without filing a formal
+        lawsuit, if handled in a just and forthright manner. After a minor
+        accident, tenant and landlord may be able to negotiate together,
+        optionally with the insurer’s
+      </p>
+    </div>
+  );
+};
+
+export default safety;
