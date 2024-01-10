a8ce9db58a974cf6d71a248400eaeaa2af1f3fef
Riaz <riazahmadbutt01@gmail.com>
2024-01-10 18:03:09 +0500

    optimize the code, added see more button, build new properties page

--- pages/public/houseRules.js
+++ pages/public/houseRules.js
@@ -1,238 +1,146 @@
-import React from "react";
-import Picture1 from "../../assets/imgs/Picture1.jpg";
-import Picture2 from "../../assets/imgs/Picture2.jpg";
+import React, { useState } from "react";
+import Picture1 from "../../assets/imgs/Picture1.png";
+import Picture2 from "../../assets/imgs/Picture2.png";
 import Picture3 from "../../assets/imgs/Picture3.png";
-import Picture16 from "../../assets/imgs/Picture16.jpg";
 import Picture4 from "../../assets/imgs/Picture4.jpg";
-import Picture6 from "../../assets/imgs/Picture6.jpg";
+import Picture5 from "../../assets/imgs/Picture5.png";
+import Picture6 from "../../assets/imgs/Picture6.png";
 import Picture7 from "../../assets/imgs/Picture7.png";
 import Picture8 from "../../assets/imgs/Picture8.jpg";
 import Picture9 from "../../assets/imgs/Picture9.jpg";
 import Picture10 from "../../assets/imgs/Picture10.jpg";
 import Picture11 from "../../assets/imgs/Picture11.jpg";
-import Picture12 from "../../assets/imgs/Picture12.jpg";
+import Picture12 from "../../assets/imgs/Picture12.png";
 import Picture13 from "../../assets/imgs/Picture13.jpg";
 import Picture14 from "../../assets/imgs/Picture14.jpg";
-import Picture17 from "../../assets/imgs/Picture17.jpg";
+import Picture15 from "../../assets/imgs/Picture15.png";
 
 const houseRules = () => {
+  const houseRulesData = [
+    {
+      img: Picture1.src,
+      description: "1. No parties or under-age drinking or events allowed",
+      expanded: false,
+    },
+    {
+      img: Picture2.src,
+      description: "2. No smoking allowed",
+      expanded: false,
+    },
+    {
+      img: Picture3.src,
+      description: "3. No pets allowed (Can Edit)",
+      expanded: false,
+    },
+    {
+      img: Picture4.src,
+      description: "4. Suitable for toddlers and children (Can Edit)",
+      expanded: false,
+    },
+    {
+      img: Picture5.src,
+      cancelImg: Picture3.src,
+      description: "5. No unregistered guests allowed",
+      expanded: false,
+    },
+    {
+      img: Picture6.src,
+      description: "6. Please don't eat or drink in the bedroom",
+      expanded: false,
+    },
+    {
+      img: Picture7.src,
+      description: "7. Please respect the noise curfew",
+      expanded: false,
+    },
+    {
+      img: Picture8.src,
+      description: "8. Please turn off the AC when you go out",
+      expanded: false,
+    },
+    {
+      img: Picture9.src,
+      description: "9. Please respect check-in and check-out times",
+      expanded: false,
+    },
+    {
+      img: Picture10.src,
+      description:
+        "10. Please take extra care of your keys and valuables. Lost keys incur a replacement fee. We are not responsible for lost or stolen valuables.",
+      expanded: false,
+    },
+    {
+      img: Picture11.src,
+      description:
+        "11. Please take care of the furnishings. You must pay for damages that exceed the security deposit.",
+      expanded: false,
+    },
+    {
+      img: Picture12.src,
+      cancelImg: Picture3.src,
+      description: "12. Please don’t rearrange the furniture",
+      expanded: false,
+    },
+    {
+      img: Picture13.src,
+      description: "13. Please do your dishes",
+      expanded: false,
+    },
+    {
+      img: Picture14.src,
+      description: "14. Please take the trash out before you leave",
+      expanded: false,
+    },
+    {
+      img: Picture15.src,
+      description: "15. No illegal substances allowed on the premises",
+      expanded: false,
+    },
+  ];
+
+  const [showMore, setShowMore] = useState(new Array(houseRulesData.length).fill(false));
+
+  const toggleExpanded = (index) => {
+    const updatedShowMore = [...showMore];
+    updatedShowMore[index] = !updatedShowMore[index];
+    setShowMore(updatedShowMore);
+  };
+
   return (
-    <div className="md:container md:mx-auto p-3">
-      <div className="flex h-screen">
+    <div className="md:container md:mx-auto">
+      <div className="flex h-screen justify-center8">
         <div className="flex-1 p-4">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <img
-                  src={Picture1.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
+            {houseRulesData.map((items, index) => (
+          <div className="p-2 flex flex-col items-center justify-center sm:justify-start sm:items-start">
+                <div className="">
+                  <img
+                    src={items.img}
+                    alt="Image 1"
+                    className="h-20 object-cover mb-4 rounded-md"
+                  />
+                </div>
+                <p className="text-gray-800">
+                {showMore[index]
+                ? items.description
+                : `${items.description.slice(0, 30)}${
+                    items.description.length > 30 ? "..." : ""
+                  }`}
+                </p>
+                {items.description.length > 30 && (
+                  <button
+                    className="btn text-sm font-medium text-gray-900 hover:text-linkColor"
+                    onClick={() => toggleExpanded(index)}
+                  >
+                    {showMore[index] ? "Show less" : "Show more"}
+                  </button>
+                )}
               </div>
-              <p className="text-gray-800">
-                1. No parties or under-age drinking or events allowed
-              </p>
-            </div>
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <img
-                  src={Picture2.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">2. No smoking allowed</p>
-            </div>
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <img
-                  src={Picture16.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">3. No pets allowed (Can Edit)</p>
-            </div>
-            <div className="p-4">
-              <div className="flex justify-center">
-                <img
-                  src={Picture4.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">
-                4. Suitable for toddlers and childrens (Can Edit)
-              </p>
-            </div>
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <h1 className="h-20 text-3xl font-black pt-3 mb-4">
-                  UNREGIS Guest
-                </h1>
-              </div>
-              <p class="text-gray-800">5. No unregistered guests allowed</p>
-            </div>
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <img
-                  src={Picture6.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">
-                6. Please don't eat or drink in the badroom
-              </p>
-            </div>
-            <div className="p-4">
-                <div className="flex justify-center">
-              <img
-                src={Picture7.src}
-                alt="Image 1"
-                className="h-20 object-cover mb-4 rounded-md"
-              />
-              </div>
-              <p className="text-gray-800">
-                7. Please respect the noise curfew
-              </p>
-            </div>
-            <div className="p-4">
-                <div className="flex justify-center">
-              <img
-                src={Picture8.src}
-                alt="Image 1"
-                className="h-20 object-cover mb-4 rounded-md"
-              />
-              </div>
-              <p className="text-gray-800">
-                8. Please turn off the AC when you go out
-              </p>
-            </div>
-            <div className="p-4">
-                <div className="flex justify-center">
-              <img
-                src={Picture9.src}
-                alt="Image 1"
-                className="h-20 object-cover mb-4 rounded-md"
-              />
-              </div>
-              <p className="text-gray-800">
-                9. Please respect check-in and check-out times
-              </p>
-            </div>
-            <div className="p-4 grid justify-items-center">
-              <div className="flex justify-center">
-                <img
-                  src={Picture10.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">
-                10. Please take extra care of your keys and valuables. Lost keys
-                incur a replacement fee. We are not responsible for lost or
-                stolen valuables.
-              </p>
-            </div>
-            <div className="p-4">
-                <div className="flex justify-center">
-              <img
-                src={Picture11.src}
-                alt="Image 1"
-                className="h-20 object-cover mb-4 rounded-md"
-              />
-              </div>
-              <p className="text-gray-800">
-                11. Please take care of the furnishings. You must pay for
-                damages which exceed the security deposit.
-              </p>
-            </div>
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <img
-                  src={Picture12.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">
-                12. Please don’t rearrange the furniture
-              </p>
-            </div>
-            <div className="p-4">
-                <div className="flex justify-center">
-              <img
-                src={Picture13.src}
-                alt="Image 1"
-                className="h-20 object-cover mb-4 rounded-md"
-              />
-              </div>
-              <p className="text-gray-800">13. Please do your dishes</p>
-            </div>
-            <div className="p-4">
-                <div className="flex justify-center">
-              <img
-                src={Picture14.src}
-                alt="Image 1"
-                className="h-20 object-cover mb-4 rounded-md"
-              />
-              </div>
-              <p className="text-gray-800">
-                14. Please take the trash out before you leave
-              </p>
-            </div>
-            <div className="p-4">
-              <div className="relative flex justify-center">
-                <img
-                  src={Picture3.src}
-                  alt="Image 3"
-                  className="h-20 object-cover absolute"
-                />
-                <img
-                  src={Picture17.src}
-                  alt="Image 1"
-                  className="h-20 object-cover mb-4 rounded-md"
-                />
-              </div>
-              <p className="text-gray-800">
-                15. No illegal substances allowed on the premises
-              </p>
-            </div>
+            ))}
           </div>
         </div>
         <div className="w-1/4 p-4 hidden sm:block">
-          <h1 className="font-bold text-6xl p-2 leading-relaxed">
+          <h1 className="font-bold text-5xl p-2 leading-relaxed">
             HERE’S A QUICK VISUAL SUMMARY OF{" "}
             <span className="underline underline-offset-2">SOME</span> of{" "}
             <span className="underline underline-offset-2">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,8 @@ const config: Config = {
     colors:{
       primary: "#F67172",
       secondary: "#FCB973",
-      gray:'#FCFCFF'
+      gray:'#FCFCFF',
+      linkColor : '#000FFF'
     }
   },
   plugins: [],
