e5416bfbcf59ab145ab8f11a98ba80377affca7d
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-01-16 17:56:20 +0500

    making header, footer ,sidebar, change Ui design and colors,

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ export const Header = () => {
 
   return (
     <nav className="bg-primary  shadow-sm  dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto ">
         <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-secondary">
             <img src={logoAircasita.src} className="w-36" />
